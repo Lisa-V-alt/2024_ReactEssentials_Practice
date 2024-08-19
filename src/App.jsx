e9c9686e2c86c34135e61854e5c32a60ed3d9f01
Lisa-V-alt <uhm.lisav@gmail.com>
2024-08-19 11:17:05 +1000

    added section for results, lifted input to app

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,36 @@
+import {useState} from 'react';
 import Header from './components/Header.jsx';
 import UserInput from './components/UserInput.jsx';
+import Results from './components/Results.jsx';
 
+
+//two-way binding & initial states (default values):
+//these have been lifted from UserInput.jsx in order to pass input to Results component
 function App() {
+  const[userInput, setUserInput] = useState({
+    initialInvestment: 10000,
+    annualInvestment: 1200,
+    expectedReturn: 6,
+    duration: 10,
+});
+
+//function that updates the default state:
+//properties are dynamically set depending on the value of inputIdentifier
+function handleChange(inputIdentifier, newValue){
+  setUserInput(prevUserInput => {
+      return{
+          ...prevUserInput,
+          [inputIdentifier]: newValue
+      };
+  });
+}
+
+//userInput results are shared to 2 different components below:
   return (
     <>
     <Header />
-    <UserInput />
+    <UserInput userInput={userInput} onChange={handleChange} />
+    <Results input={userInput} />
     </>
   );
 }
--- src/components/UserInput.jsx
+++ src/components/UserInput.jsx
@@ -1,39 +1,19 @@
-import {useState} from 'react';
-
-export default function UserInput(){
-//two-way binding & initial states (default values):
-const[userInput, setUserInput] = useState({
-    initialInvestment: 10000,
-    annualInvestment: 1200,
-    expectedReturn: 6,
-    duration: 10,
-});
-
-//function that updates the default state:
-//properties are dynamically set depending on the value of inputIdentifier
-function handleChange(inputIdentifier, newValue){
-    setUserInput(prevUserInput => {
-        return{
-            ...prevUserInput,
-            [inputIdentifier]: newValue
-        };
-    });
-}
+export default function UserInput({onChange, userInput}){
 
 //user input fields:
-    return <section id="user-input">
+    return( <section id="user-input">
     <div className="input-group">
         <p>
             <label>Initial Investment</label>
             <input type="number" required  
             value={userInput.initialInvestment}
-            onChange={(event)=> handleChange('initialInvestment', event.target.value)}/>
+            onChange={(event)=> onChange('initialInvestment', event.target.value)}/>
         </p>
         <p>
             <label>Annual Investment</label>
             <input type="number" required 
             value={userInput.annualInvestment}
-            onChange={(event)=> handleChange('annualInvestment', event.target.value)}/>
+            onChange={(event)=> onChange('annualInvestment', event.target.value)}/>
         </p>
     </div>
     <div className="input-group">
@@ -41,14 +21,15 @@ function handleChange(inputIdentifier, newValue){
             <label>Expected Return</label>
             <input type="number" required 
             value={userInput.expectedReturn}
-            onChange={(event)=> handleChange('expectedReturn', event.target.value)}/>
+            onChange={(event)=> onChange('expectedReturn', event.target.value)}/>
         </p>
         <p>
             <label>Duration</label>
             <input type="number" required 
             value={userInput.duration}
-            onChange={(event)=> handleChange('duration', event.target.value)}/>
+            onChange={(event)=> onChange('duration', event.target.value)}/>
         </p>
     </div>
     </section>
+    );
 }
